fb18185f16645cef423809463144dc8e87722bf3
MidhunBh <83406961+MidhunBh@users.noreply.github.com>
2026-07-20 13:17:32 +0200

    C2ST-MMD baseline MNIST complete — Table 3 ballpark match

--- Experiments/c2st_mnist.py
+++ Experiments/c2st_mnist.py
@@ -90,7 +90,7 @@ dataloader_real_all = torch.utils.data.DataLoader(
     datasets.MNIST(
         "./data/mnist",
         train=True,
-        download=True,
+        download=False,
         transform=transforms.Compose(
             [transforms.Resize(img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         ),
